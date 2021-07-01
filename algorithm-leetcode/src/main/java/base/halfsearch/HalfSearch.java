936746ffcc1ffb9a872802d20d1ff9bf0cde78ae
majingyuan <imcking@126.com>
2021-07-01 14:21:35 +0800

    Create HalfSearch.java

---
+++ algorithm-leetcode/src/main/java/base/halfsearch/HalfSearch.java
@@ -0,0 +1,22 @@
+package base.halfsearch;
+
+public class HalfSearch {
+    public boolean search(int[] array,int target){
+        int left =0;
+        int right=array.length-1;
+        while(left <= right){
+            int mid = left+(right-left)/2;
+
+            if (array[mid] == target){
+                return true;
+            }else if(array[mid] <target){
+                left =  mid+1;
+            }else{
+                right = mid -1;
+            }
+        }
+
+
+        return false;
+    }
+}
